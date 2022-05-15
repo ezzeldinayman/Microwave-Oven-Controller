12ce42098d2d743d7f2b49dd928b40d03751447e
ezzeldinayman <ezz_ayman@hotmail.com>
2022-05-15 22:06:14 +0200

    Delay Functions Added

---
+++ Delay.c
@@ -0,0 +1,36 @@
+#include "standard_macros.h"
+#include "tm4c123gh6pm.h"
+#include "Delay.h"
+
+void _delay_init(void)
+{
+	SYSCTL_RCGCTIMER_R |= 1;
+}
+void _delay_OneMs(void)
+{ 
+		CLR_BIT(TIMER0_CTL_R,0);
+		TIMER0_CFG_R = 0x00;						
+		SET_BIT(TIMER0_TAMR_R,1); 	
+		CLR_BIT(TIMER0_TAMR_R,4);
+		TIMER0_TAILR_R = 79999;	
+		SET_BIT(TIMER0_CTL_R,0);	
+		while(1)
+		{
+			if(READ_BIT(TIMER0_RIS_R,0) == 1)
+			{
+				SET_BIT(TIMER0_ICR_R,0);
+				TOG_BIT(TIMER0_MIS_R,2);
+				break;
+			}
+		}
+}
+void _delay_ms(int delay)
+{
+	_delay_init();
+	int i;
+	for(i=0;i<delay;i++)
+	{
+		_delay_OneMs();
+	}
+}
+
---
+++ Delay.h
@@ -0,0 +1,8 @@
+#ifndef DELAY_H_
+#define DELAY_H_
+
+void _delay_init(void);
+void _delay_OneMs(void);
+void _delay_ms(int delay);
+
+#endif
