44cfe1240fb43f9bfe408b8c9ffb980ae77d801c
Bido5 <1901685@eng.asu.edu.eg>
2022-05-21 00:19:22 +0200

    Keypad.c update

--- Keypad.c
+++ Keypad.c
@@ -1,41 +1,50 @@
 #include "ARM_DIO.h"
-#define KPORT 'B'
+#define KPORT1 'B'
+#define KPORT2 'E'
 #include "Keypad.h"
+
 void Keypad_vInit()
 {
-		DIO_vInitPort(KPORT);
-		DIO_vSetPortDir(KPORT,0x0F); //P4 to P7 are input & P0 to P3 are output
-		DIO_vEnablePullUp(KPORT,7);
-		DIO_vEnablePullUp(KPORT,6);
-		DIO_vEnablePullUp(KPORT,5);
-		DIO_vEnablePullUp(KPORT,4);
+        DIO_vInitPort(KPORT1);
+        DIO_vInitPort(KPORT2);
+        DIO_vSetPortDir(KPORT1,0x0F);  //PB0 --> PB3 : ROWS (OUTPUT)
+        DIO_vSetPinDir(KPORT2,1,0);
+		DIO_vSetPinDir(KPORT2,2,0);
+		DIO_vSetPinDir(KPORT2,3,0);
+		DIO_vSetPinDir(KPORT2,4,0);
+        DIO_vEnablePullUp(KPORT2,1);
+        DIO_vEnablePullUp(KPORT2,2);
+        DIO_vEnablePullUp(KPORT2,3);
+        DIO_vEnablePullUp(KPORT2,4);
+        DIO_vWritePort(KPORT1,0x0F);
 }
 
 
 unsigned char Keypad_u8Read()
 {
-		unsigned char ascii[4][4] = {
-																{'1','2','3','A'},
-																{'4','5','6','B'},
-																{'7','8','9','C'},
-																{'*','0','#','D'}
-																};
-		unsigned char row,col,x;
-		char return_value = 0xFF;
-		for(row=0;row<4;row++)
-		{
-			DIO_vWriteLowNibble(KPORT,0xFF);
-			DIO_vWritePin(KPORT,row,0);
-			for(col=0;col<4;col++)
-			{
-					x = DIO_u8ReadPin(KPORT,col+4);
-					if(x==0)
-					{
-							return_value=ascii[row][col];
-							break;
-					}
-			}
-			
-		}	
-		return return_value;	
+        unsigned char ascii[4][4] = {
+                                                                {'1','2','3','A'},
+                                                                {'4','5','6','B'},
+                                                                {'7','8','9','C'},
+                                                                {'*','0','#','D'}
+                                                                };
+        unsigned char row,col,x;
+        char return_value = 0xFF;
+        for(row=0;row<4;row++)
+        {
+            DIO_vWriteLowNibble(KPORT1,0xFF);
+            DIO_vWritePin(KPORT1,row,0);
+            for(col=0;col<4;col++)
+            {
+                    x = DIO_u8ReadPin(KPORT2,col+1);
+                    if(x==0)
+                    {
+                            return_value=ascii[row][col];
+                            break;
+                    }
+            }
+
+        }
+        return return_value;
 }
+
