ae7320f7b3a3699cafc401597c24b10de2a2e3a7
Bido5 <1901685@eng.asu.edu.eg>
2022-05-22 23:05:32 +0200

    Buzzer added

---
+++ Buzzer.h
@@ -0,0 +1,10 @@
+#ifndef BUZZER_H_
+#define BUZZER_H_
+
+
+void Buzzer_vInit(unsigned char portname, unsigned char pinnumber);
+void Buzzer_On(unsigned char portname, unsigned char pinnumber);
+void Buzzer_Off(unsigned char portname, unsigned char pinnumber);
+
+
+#endif
