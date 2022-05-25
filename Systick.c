e797854cc55f578766f95447b56169cb6bbc3ade
Ezz Eldin Ayman <63506700+ezzeldinayman@users.noreply.github.com>
2022-05-25 18:52:29 +0200

    Update Systick.c

--- Systick.c
+++ Systick.c
@@ -5,7 +5,7 @@
 void Systick_vInitInterrupt(void)
 {
 	NVIC_ST_CTRL_R = 0; //disable systick during setup
-	NVIC_ST_RELOAD_R = 1599999;
+	NVIC_ST_RELOAD_R = 159999;
 	NVIC_ST_CURRENT_R = 0;	//clears any current value
 	SET_BIT(NVIC_ST_CTRL_R,0);
 	SET_BIT(NVIC_ST_CTRL_R,2);
